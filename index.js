d2d5a57790558173d712c9f90c7d4eee494712eb
Michael Paulukonis <xraysmalevich@gmail.com>
2017-02-27 21:31:24 -0500

    zip work, tests, and possibly post-install zippage (under construction)

--- index.js
+++ index.js
@@ -29,13 +29,30 @@ let Corpora = function() {
   let fs = require(`fs`),
       path = require(`path`),
       zipkit = require(`node-zipkit`),
+      mkdirp = require(`mkdirp`),
       unzipLoc = path.join(__dirname, `./unzip-temp`),
       debreak = require(`./lib/debreak`),
       textutil = require(`./lib/textutil`),
-      // root = path.join(__dirname, `./corpus.zip`),
-      root = path.join(__dirname, `./corpus`),
+      root = path.join(__dirname, `./zipped-corp`),
+      // root = path.join(__dirname, `./corpus`),
       books = walkSync(root),
       texts = [],
+      // uncompress is poorly named and/or poorly written
+      // it returns a filename, uncompressing it as a side-effect
+      uncompress = function(filename) {
+        if (filename.match(/.(zip|7z)$/)) {
+          let unzipName = filename.replace(root, unzipLoc).replace(/\.(zip|7z)$/, ``),
+              targPath = path.dirname(unzipName);
+          if (!fs.existsSync(unzipName)) {
+            if (!fs.existsSync(targPath)) {
+              mkdirp(targPath);
+            }
+            zipkit.unzipSync(`"${filename}"`, targPath);
+          }
+          filename = unzipName;
+        }
+        return filename;
+      },
       cleanName = (name) => name.replace(/.(txt|js|zip)$/g, ``)
         .replace(root, ``)
         .replace(/^\/|\\/, ``),
@@ -43,16 +60,7 @@ let Corpora = function() {
         // if zip file, unzip it, and retrieve the zipped file
         // if unzip folder does not exist, create it
         // if unzipped file already exists, do not unzip!
-        if (filename.match(/zip$/)) {
-          let unzipName = filename.replace(root, unzipLoc).replace(/\.zip$/, ``);
-          if (!fs.existsSync(unzipName)) {
-            if (!fs.existsSync(unzipLoc)) {
-              fs.makeDir(unzipLoc);
-            }
-            zipkit.unzipSync(`"${filename}"`, unzipLoc);
-          }
-          filename = unzipName;
-        }
+        filename = uncompress(filename);
         let text = fs.readFileSync(filename),
             iconv = require(`iconv-lite`),
             book = iconv.decode(new Buffer(text), `ISO=8859-1`);
@@ -68,11 +76,13 @@ let Corpora = function() {
     // only "problem" here is that we read the text EVERY TIME we need it
     // but... if we need it only once, that's not a problem, so... leave it for now
     if (filename.indexOf(`sentences`) > -1) {
-      var sentences = require(filename);
-      texts.push({name: cleanName(filename), text: () => sentences.join(`\n`), sentences: () => sentences});
+      texts.push({name: cleanName(filename),
+        text: () => require(uncompress(filename)).join(`\n`),
+        sentences: () => require(uncompress(filename))});
     } else {
-          // let sentences = textutil.sentencify(opts.text),
-      texts.push({name: cleanName(filename), text: () => gettext(filename), sentences: () => textutil.sentencify(gettext(filename)) });
+      texts.push({name: cleanName(filename),
+        text: () => gettext(filename),
+        sentences: () => textutil.sentencify(gettext(filename)) });
     }
   }
 
--- test/corpora.provider.tests.js
+++ test/corpora.provider.tests.js
@@ -7,6 +7,10 @@
       Corpora = require(`../index.js`),
       newcorpora = new Corpora();
 
+  // setup should make sure that the unzip-temp folder is empty
+  // so, perhaps that should be exposed by the library?
+  // or just make it a parallel dependency?
+
   describe(`Corpora tests`, function() {
 
     describe(`API tests`, function() {
@@ -21,6 +25,9 @@
         expect(corpora).to.be.a(`object`);
         expect(corpora).to.be.an.instanceof(Corpora);
       });
+
+      // also exposes readFile (does anything use this?), cleanName, and filter
+      // TODO: tests for these
 
       it(`should expose an array of texts`, function() {
         expect(newcorpora.texts).to.be.instanceOf(Array);
@@ -37,23 +44,47 @@
       it(`texts should have a sentences property that is a function`, function() {
         expect(newcorpora.texts[0].sentences).to.be.a(`function`);
       });
+    });
 
+    describe(`functional tests`, function() {
       // there is an implicit test here, that we access both the text and the sentences of the same object
       // and get each of them
       // in an earlier iteration, only the first attempt would succeed
       // perhaps these should be THREE separate tests, on 3 separate corpora objects?
       // picked exclusively (then we have to deal with THAT issue, which isn't SOOOO bad, no?)
 
-      it(`texts should have a text property that is a function and returns a string`, function() {
+      it(`text property returns a string`, function() {
         expect(newcorpora.texts[0].text()).to.be.a(`string`);
       });
 
-      it(`texts should have a sentences property that is a function and returns an array of strings`, function() {
-        this.timeout(10000); // OUCH! A History of Art for Beginners..." takes > 5 seconds....
+      it(`sentences property returns an array of strings`, function() {
+        this.timeout(10000); // OUCH! A History of Art for Beginners..." takes ~= 8..10 seconds....
         let sentences = newcorpora.texts[0].sentences(),
             firstSent = sentences[0];
         expect(sentences).to.be.instanceOf(Array);
         expect(firstSent).to.be.a(`string`);
+      });
+
+      // TODO: test special sentences folders
+
+      // TODO: test filter
+      it(`special sentences files function like other texts`, function() {
+        let blob = newcorpora.filter(`sentences`)[0];
+        expect(blob).to.be.an(`object`);
+
+        describe(`specicial sentneces provide text and sentences normally`, function() {
+          it(`text property returns a string`, function() {
+            expect(blob.text()).to.be.a(`string`);
+          });
+
+          it(`sentences property returns an array of strings`, function() {
+            let sentences = blob.sentences(),
+                firstSent = sentences[0];
+            expect(sentences).to.be.instanceOf(Array);
+            expect(firstSent).to.be.a(`string`);
+          });
+        });
+
       });
 
     });
